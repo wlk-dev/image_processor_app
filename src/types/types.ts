b4a05343fd741b2fd0f57a0e3220f2d892431ca1
William Koelling <williamkoelling@Williams-MacBook-Pro-2.local>
2024-10-04 11:47:39 -0500

    added ImageContext & refactored types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,25 +1,49 @@
-type P = 'w' | 'h' | 'fit' | 'auto';
+import React from "react";
 
-type BasicParams = {
+const FlipOptions = {
+    Horizontal: 'h' as const,
+    Vertical: 'v' as const,
+    Both: 'hv' as const,
+} as const;
+
+type FlipOption = typeof FlipOptions[keyof typeof FlipOptions];
+
+export type AllowedFlip = FlipOption
+export type AllowedValues = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 90 | 180 | 270;
+
+type P = 'w' | 'h' | 'fit' | 'auto';
+type Parameters = {
     [p in P]: string | number;
 }
 
-export type ImageParams = BasicParams & {
-    flip?: 'hv' | 'h' | 'v';
+export type ImageParameters = Parameters & {
+    flip?: AllowedFlip;
     orient?: number;
-    rot?: number;
+    rot?: number
 }
 
 export type Image = {
     url: string;
     name: string;
-    params: ImageParams;
-    paramUrl?: string;
+    parameters: ImageParameters;
+    parameterUrl?: string;
 }
 
 export type Index = number;
 
 export type ImageListProps = {
     images: Image[];
-    onSelectImage: (image: Image, index: Index) => void;
+    onSelectImage: (index: Index) => void;
+}
+
+export type ImageModalProps = {
+    // image: Image;
+    index: Index;
+    options: AllowedValues[]
+}
+
+export type FetchImageProps = {
+    setImages: React.Dispatch<React.SetStateAction<Image[]>>;
+    setError: React.Dispatch<React.SetStateAction<string | null>>;
+    imgixParameters: ImageParameters;
 }
